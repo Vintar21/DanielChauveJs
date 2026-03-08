2606f766cafd4a1bc2832a15ea3473440d688c2a
Vintar21 <vintar21@gmail.com>
2026-03-08 15:21:02 +0100

    refactor: create abstract class to encapsulate basic methods of TwitchClient

---
+++ src/twitch/ATwitchClient.ts
@@ -0,0 +1,173 @@
+import {
+  ApiClient,
+  HelixBitsApi,
+  HelixChannelApi,
+  HelixChannelPointsApi,
+  HelixCharityApi,
+  HelixChatApi,
+  HelixClipApi,
+  HelixGameApi,
+  HelixGoalApi,
+  HelixHypeTrainApi,
+  HelixModerationApi,
+  HelixPollApi,
+  HelixPredictionApi,
+  HelixUser,
+  HelixUserApi,
+} from "@twurple/api";
+import { Bot, MessageEvent } from "@twurple/easy-bot";
+import ChannelPointsListener from "../channel-points-rewards/ChannelPointsListener";
+import CommandsManager from "../commands/CommandsManager";
+import { channel } from "../config/ConfigLoader";
+import CountersManager from "../counters/CountersManager";
+import TimerManager from "../timers/TimerManager";
+import { EMPTY, SPACE } from "../utils/StringConstants";
+import TwitchClient from "./TwitchClient";
+
+export default abstract class ATwitchClient {
+  protected static botApp: Bot;
+  protected static broadcasterApp: Bot;
+  protected static broadcaster: HelixUser;
+
+  static commandsManager: CommandsManager;
+  static channelPointsListener: ChannelPointsListener;
+  static timerManager: TimerManager = TimerManager.getInstanceAndInit();
+  static countersManager: CountersManager;
+
+  public abstract init(assignHandlers: boolean): Promise<void>;
+
+  public createMarker(description?: string): Promise<boolean> {
+    if (ATwitchClient.broadcaster) {
+      return ATwitchClient.broadcaster.getStream().then((stream) => {
+        if (ATwitchClient.broadcaster && stream && stream !== null) {
+          this.getApi().streams.createStreamMarker(
+            ATwitchClient.broadcaster?.id,
+            description,
+          );
+          return true;
+        }
+        return false;
+      });
+    }
+    return Promise.resolve(false);
+  }
+
+  // Listeners/Handlers
+  public async shoutout(userId: string): Promise<void> {
+    const currentStream = await this.getBroadcaster().getStream();
+    if (!currentStream || currentStream === null || !currentStream.startDate) {
+      const user = await this.getUsersApi().getUserById(userId);
+      const username = user === null ? EMPTY : user.name;
+      ATwitchClient.send(
+        `Merci pour le raid ${username}${username === EMPTY ? EMPTY : SPACE}! Même si l'autre est pas en stream LUL`,
+      );
+      return;
+    }
+    return this.getChatApi().shoutoutUser(this.getBroadcasterId(), userId);
+  }
+
+  public static send(message: String, isAnnounce: boolean = false): void {
+    if (isAnnounce) {
+      ATwitchClient.botApp.announce(channel, message.toString());
+    } else {
+      ATwitchClient.botApp.say(channel, message.toString());
+    }
+  }
+
+  public reply(message: String, event: MessageEvent): void {
+    // use bot.reply instead ? How ?
+    event.reply(message.toString());
+  }
+
+  /*----- API calls -----*/
+
+  // TODO better handling of error (maybe in the mainApp ?)
+  public getApi(): ApiClient {
+    if (!ATwitchClient.broadcasterApp) {
+      throw Error("Bot isn't available, API couldn't be accessed");
+    }
+    return ATwitchClient.broadcasterApp?.api;
+  }
+
+  public getBitsApi(): HelixBitsApi {
+    return this.getApi().bits;
+  }
+
+  public getChannelsApi(): HelixChannelApi {
+    return this.getApi().channels;
+  }
+
+  public getCharityApi(): HelixCharityApi {
+    return this.getApi().charity;
+  }
+
+  public getChatApi(): HelixChatApi {
+    return this.getApi().chat;
+  }
+
+  public getChannelPointsApi(): HelixChannelPointsApi {
+    return this.getApi().channelPoints;
+  }
+
+  public getClipApi(): HelixClipApi {
+    return this.getApi().clips;
+  }
+
+  public getGamesApi(): HelixGameApi {
+    return this.getApi().games;
+  }
+
+  public getGoalApi(): HelixGoalApi {
+    return this.getApi().goals;
+  }
+
+  public getHypeTrainApi(): HelixHypeTrainApi {
+    return this.getApi().hypeTrain;
+  }
+
+  public getModerationApi(): HelixModerationApi {
+    return this.getApi().moderation;
+  }
+
+  public getPollsApi(): HelixPollApi {
+    return this.getApi().polls;
+  }
+
+  public getPredictionApi(): HelixPredictionApi {
+    return this.getApi().predictions;
+  }
+
+  public getUsersApi(): HelixUserApi {
+    return this.getApi().users;
+  }
+
+  public getBroadcaster(): HelixUser {
+    return ATwitchClient.broadcaster;
+  }
+
+  public getBroadcasterId(): string {
+    return this.getBroadcaster().id;
+  }
+
+  public getBroadcasterApp(): Bot {
+    return ATwitchClient.broadcasterApp;
+  }
+
+  public static getCommandsManager(): CommandsManager {
+    return ATwitchClient.commandsManager;
+  }
+
+  public static getChannelPointsListener(): ChannelPointsListener {
+    return ATwitchClient.channelPointsListener;
+  }
+
+  public static getTimerManager(): TimerManager {
+    return ATwitchClient.timerManager;
+  }
+
+  public async getCurrentGame(): Promise<string | undefined> {
+    return this.getChannelsApi()
+      .getChannelInfoById(this.getBroadcasterId())
+      .then((channel) => channel?.gameName);
+  }
+}
--- src/twitch/TwitchClient.ts
+++ src/twitch/TwitchClient.ts
@@ -1,19 +1,9 @@
 import {
-  ApiClient,
-  HelixChannelApi,
-  HelixChatApi,
-  HelixModerationApi,
-  HelixPollApi,
-  HelixPredictionApi,
-  HelixUser,
-  HelixUserApi,
-} from "@twurple/api";
-import {
   RefreshingAuthProvider,
   refreshUserToken,
   StaticAuthProvider,
 } from "@twurple/auth";
-import { Bot, MessageEvent, RaidEvent } from "@twurple/easy-bot";
+import { Bot, RaidEvent } from "@twurple/easy-bot";
 import { EventSubStreamOnlineEvent } from "@twurple/eventsub-base/lib/events/EventSubStreamOnlineEvent";
 import { EventSubWsListener } from "@twurple/eventsub-ws";
 import { MainApp } from "../app";
@@ -31,22 +21,15 @@ import {
 import CountersManager from "../counters/CountersManager";
 import TimerManager from "../timers/TimerManager";
 import { log, minutes, seconds, warn } from "../utils/CommonUtils";
-import { EMPTY, SPACE } from "../utils/StringConstants";
+import ATwitchClient from "./ATwitchClient";
 import { onMessage } from "./TwitchEventHandlers";
 
-export default class TwitchClient {
-  private static botApp: Bot;
-  private static broadcasterApp: Bot;
-  private static broadcaster: HelixUser;
-
-  static commandsManager: CommandsManager;
-  static channelPointsListener: ChannelPointsListener;
-  static timerManager: TimerManager = TimerManager.getInstanceAndInit();
-  static countersManager: CountersManager;
-
+export default class TwitchClient extends ATwitchClient {
   private static INSTANCE: TwitchClient;
 
-  constructor() {}
+  constructor() {
+    super();
+  }
 
   /* TODO
    * - Create a hierarchy to split this class
@@ -62,7 +45,7 @@ export default class TwitchClient {
     return TwitchClient.INSTANCE;
   }
 
-  public async init(assignHandlers: boolean = false) {
+  public async init(assignHandlers: boolean = false): Promise<void> {
     // Authentication of broadcasterApp
     const broadcasterAccessToken = await refreshUserToken(
       broadcasterClientId,
@@ -78,13 +61,13 @@ export default class TwitchClient {
 
     broadcasterAuthProvider.addUserForToken(broadcasterAccessToken, ["chat"]);
 
-    TwitchClient.broadcasterApp = new Bot({
+    ATwitchClient.broadcasterApp = new Bot({
       authProvider: broadcasterAuthProvider,
       channels: [channel],
     });
 
     // Authentication of botApp
-    TwitchClient.botApp = TwitchClient.broadcasterApp;
+    ATwitchClient.botApp = ATwitchClient.broadcasterApp;
     // TODO: modif in config to don't have the fallback twice
     if (botClientId && botAccessToken) {
       // TODO: Limit bot rights ? Just need to send message as broadcaster has all other rights ?
@@ -93,7 +76,7 @@ export default class TwitchClient {
         botAccessToken,
       );
 
-      TwitchClient.botApp = new Bot({
+      ATwitchClient.botApp = new Bot({
         authProvider: botAuthProvider,
         channels: [channel],
       });
@@ -103,31 +86,29 @@ export default class TwitchClient {
 
     if (_broadcaster === null) return;
 
-    TwitchClient.broadcaster = _broadcaster;
-
-    // Who init the OBSManager ? Should definitely not be here
+    ATwitchClient.broadcaster = _broadcaster;
 
     // ChannelPointListener
-    TwitchClient.channelPointsListener =
+    ATwitchClient.channelPointsListener =
       await ChannelPointsListener.getInstanceAndInit(this);
 
     // TimerManager
-    TwitchClient.timerManager = TimerManager.getInstanceAndInit();
-    TwitchClient.timerManager.startAllTimers();
+    ATwitchClient.timerManager = TimerManager.getInstanceAndInit();
+    ATwitchClient.timerManager.startAllTimers();
 
     // TwitchCommandsManager
-    TwitchClient.commandsManager = CommandsManager.getInstanceAndInit();
+    ATwitchClient.commandsManager = CommandsManager.getInstanceAndInit();
 
     // CountersManager
-    TwitchClient.countersManager = CountersManager.initAllCounters();
+    ATwitchClient.countersManager = CountersManager.initAllCounters();
     allCounterCommands.forEach((command) => command.initCountersMapIfEmpty());
 
     if (assignHandlers) {
       // TODO not onMessage but onChatMessage...
       // + TwitchEventHandler may not be sufficient do it directly in this client
-      TwitchClient.botApp.onMessage(onMessage);
+      ATwitchClient.botApp.onMessage(onMessage);
 
-      TwitchClient.botApp.onRaid(this.onRaid);
+      ATwitchClient.botApp.onRaid(this.onRaid);
       const onStreamListener = new EventSubWsListener({
         apiClient: this.getApi(),
       });
@@ -140,111 +121,6 @@ export default class TwitchClient {
       );
     }
     log("Twitch Client ready !");
-  }
-
-  // TODO better handling of error (maybe in the mainApp ?)
-  public getApi(): ApiClient {
-    if (!TwitchClient.broadcasterApp) {
-      throw Error("Bot isn't available, API couldn't be accessed");
-    }
-    return TwitchClient.broadcasterApp?.api;
-  }
-
-  public createMarker(description?: string): Promise<boolean> {
-    if (TwitchClient.broadcaster) {
-      return TwitchClient.broadcaster.getStream().then((stream) => {
-        if (TwitchClient.broadcaster && stream && stream !== null) {
-          this.getApi().streams.createStreamMarker(
-            TwitchClient.broadcaster?.id,
-            description,
-          );
-          return true;
-        }
-        return false;
-      });
-    }
-    return Promise.resolve(false);
-  }
-
-  public getUsersApi(): HelixUserApi {
-    return this.getApi().users;
-  }
-
-  public getChannelsApi(): HelixChannelApi {
-    return this.getApi().channels;
-  }
-
-  public getChatApi(): HelixChatApi {
-    return this.getApi().chat;
-  }
-
-  public getModerationApi(): HelixModerationApi {
-    return this.getApi().moderation;
-  }
-
-  public getPollsApi(): HelixPollApi {
-    return this.getApi().polls;
-  }
-
-  public getPredictionApi(): HelixPredictionApi {
-    return this.getApi().predictions;
-  }
-
-  public getBroadcaster(): HelixUser {
-    return TwitchClient.broadcaster;
-  }
-
-  public getBroadcasterId(): string {
-    return this.getBroadcaster().id;
-  }
-
-  public getBroadcasterApp(): Bot {
-    return TwitchClient.broadcasterApp;
-  }
-
-  public static getCommandsManager(): CommandsManager {
-    return TwitchClient.commandsManager;
-  }
-
-  public static getChannelPointsListener(): ChannelPointsListener {
-    return TwitchClient.channelPointsListener;
-  }
-
-  public static getTimerManager(): TimerManager {
-    return TwitchClient.timerManager;
-  }
-
-  public async getCurrentGame(): Promise<string | undefined> {
-    return this.getChannelsApi()
-      .getChannelInfoById(this.getBroadcasterId())
-      .then((channel) => channel?.gameName);
-  }
-
-  public static send(message: String, isAnnounce: boolean = false) {
-    if (isAnnounce) {
-      TwitchClient.botApp.announce(channel, message.toString());
-    } else {
-      TwitchClient.botApp.say(channel, message.toString());
-    }
-  }
-
-  public reply(message: String, event: MessageEvent) {
-    // use bot.reply instead ? How ?
-    event.reply(message.toString());
-  }
-
-  // Listeners/Handlers
-  private async shoutout(userId: string): Promise<void> {
-    const currentStream = await this.getBroadcaster().getStream();
-    if (!currentStream || currentStream === null || !currentStream.startDate) {
-      const user = await this.getUsersApi().getUserById(userId);
-      const username = user === null ? EMPTY : user.name;
-      TwitchClient.send(
-        `Merci pour le raid ${username}${username === EMPTY ? EMPTY : SPACE}! Même si l'autre est pas en stream LUL`,
-      );
-      return;
-    }
-    return this.getChatApi().shoutoutUser(this.getBroadcasterId(), userId);
   }
 
   private async futureShoutout() {
@@ -310,7 +186,7 @@ export default class TwitchClient {
       await MainApp.obsManager.connect();
     }
     // In fact, no need to reaffect CommandManager
-    TwitchClient.commandsManager = CommandsManager.getInstanceAndInit();
+    ATwitchClient.commandsManager = CommandsManager.getInstanceAndInit();
     TwitchClient.raidersIdWaiting = [];
     TwitchClient.shoutedOutIds.clear();
 
@@ -321,17 +197,16 @@ export default class TwitchClient {
       setTimeout(async () => {
         log("Try resending live announce");
         MainApp.discordClient.sendLiveAnounce(
-          await TwitchClient.broadcaster.getStream(),
-          TwitchClient.broadcaster,
+          await ATwitchClient.broadcaster.getStream(),
+          ATwitchClient.broadcaster,
         );
       }, minutes(1));
     } else {
       log("Sending live announce directly");
-      MainApp.discordClient.sendLiveAnounce(stream, TwitchClient.broadcaster);
-
-      TwitchClient.send(
-        "Je suis toujours en phase de test, n'hésitez pas à me mettre à l'épreuve !",
-      );
+      MainApp.discordClient.sendLiveAnounce(stream, ATwitchClient.broadcaster);
     }
+    ATwitchClient.send(
+      "Je suis toujours en phase de test, n'hésitez pas à me mettre à l'épreuve !",
+    );
   }
 }
