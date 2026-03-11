df77de8797d1884e89e5489eaa0f376af5c1edcc
Vintar21 <vintar21@gmail.com>
2026-03-11 19:25:24 +0100

    fix: fixing lurk command

--- src/commands/simple/LurkCommand.ts
+++ src/commands/simple/LurkCommand.ts
@@ -1,11 +1,8 @@
-import { Placeholders } from "../../utils/CommandsUtils";
 import CommandOptions from "../options/CommandOptions";
 import SimpleCommand from "../templates/SimpleCommand";
 
-const options: CommandOptions = new CommandOptions([/lurk(ing)?/i])
-  .setReplyToUser(false)
-  .hasPlaceholders();
-const answer: String = `${Placeholders.USER} disparaît en lurk`;
+const options: CommandOptions = new CommandOptions([/lurk(ing)?/i]);
+const answer: String = `Bon lurk à toi !`;
 
 export default class DiscordCommand extends SimpleCommand {
   constructor(enabled: boolean = true) {
